ff9247009db4494a2e80255d8210ecafc6dc4395
naowal <naowalzaza@gmail.com>
2017-11-09 22:37:40 +0700

    final push 9/11 10-37 PM

--- cmd/gonews/main.go
+++ cmd/gonews/main.go
@@ -3,23 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
+	//"os"
 
 	"github.com/naowal/gonews/pkg/app"
 	"github.com/naowal/gonews/pkg/model"
 )
 
 const (
-	port = ":8080"
-	// mongoURL = "mongodb://127.0.0.1:27017"
-	mongoURL = "mongodb://naowal:s00446204@ds251985.mlab.com:51985/gonews"
+	port     = ":8080"
+	mongoURL = "mongodb://127.0.0.1:27017"
+	// mongoURL = "mongodb://naowal:s00446204@ds251985.mlab.com:51985/gonews"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+	//port := os.Getenv("PORT")
+	//if port == "" {
+	//	port = "8080"
+	//}
 	mux := http.NewServeMux()
 	app.Mount(mux)
 	err := model.Init(mongoURL)
